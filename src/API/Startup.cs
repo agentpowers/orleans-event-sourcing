5c783da3d363e7fe343e18d8084d47e7659c9626
Roney George <georgeroney@hotmail.com>
2018-10-24 19:55:30 -0400

    initial

---
+++ src/API/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Orleans;
+using Orleans.Configuration;
+using GrainInterfaces;
+//using Orleans.Hosting;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IClusterClient>(CreateClusterClient);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private IClusterClient CreateClusterClient(IServiceProvider serviceProvider)
+        {
+            var log = serviceProvider.GetService<ILogger<Startup>>();
+
+            // TODO replace with your connection string
+            //const string connectionString = "YOUR_CONNECTION_STRING_HERE";
+            var client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IValueGrain).Assembly))
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "orleans-docker";
+                    options.ServiceId = "AspNetSampleApp";
+                })
+                //.UseAzureStorageClustering(options => options.ConnectionString = connectionString)
+                .Build();
+
+            client.Connect(RetryFilter).GetAwaiter().GetResult();
+            return client;
+
+            async Task<bool> RetryFilter(Exception exception)
+            {
+                log?.LogWarning("Exception while attempting to connect to Orleans cluster: {Exception}", exception);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return true;
+            }
+        }
+    }
+}
---
+++ src/Silo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+    class Program
+    {
+        private static ISiloHost silo;
+        private static readonly ManualResetEvent siloStopped = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            // TODO replace with your connection string
+            silo = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "orleans-docker";
+                    options.ServiceId = "AspNetSampleApp";
+                })
+                //.UseAzureStorageClustering(options => options.ConnectionString = connectionString)
+                //.ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ValueGrain).Assembly).WithReferences())
+                .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning).AddConsole())
+                .Build();
+
+            Task.Run(StartSilo);
+
+            AssemblyLoadContext.Default.Unloading += context =>
+            {
+                Task.Run(StopSilo);
+                siloStopped.WaitOne();
+            };
+
+            siloStopped.WaitOne();
+        }
+
+        private static async Task StartSilo()
+        {
+            await silo.StartAsync();
+            Console.WriteLine("Silo started");
+        }
+
+        private static async Task StopSilo()
+        {
+            await silo.StopAsync();
+            Console.WriteLine("Silo stopped");
+            siloStopped.Set();
+        }
+    }
+}
