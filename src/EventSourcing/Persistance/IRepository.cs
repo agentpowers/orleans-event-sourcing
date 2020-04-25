2d9294dcce4d199dd34eccbe764f77166acf261d
Roney George <georgeroney@hotmail.com>
2020-04-24 23:54:43 -0400

    re orged

--- src/EventSourcing/Persistance/IRepository.cs
+++ src/EventSourcing/Persistance/IRepository.cs
@@ -6,16 +6,16 @@ namespace EventSourcing.Persistance
     {
         Task<AggregateEvent[]> GetAggregateEvents(string aggregateName, long eventId);
         Task<AggregateEvent> GetLastAggregateEvent(string aggregateName);
-        Task<(Snapshot, Event[])> GetSnapshotAndEvents(string aggregateName, long aggregateId);
-        Task<long> GetSnapshotAggregateVersion(string aggregateName, long aggregateId);
+        Task<AggregateEvent[]> GetAggregateEventsByAggregateTypeName(string aggregateName, string aggregateTypName, long aggregateVersion);
         Task<Aggregate> GetAggregate(long id, string type);
         Task<Aggregate> GetAggregateByTypeName(string type);
         Task<Aggregate[]> GetAggregatesByTypeName(string type);
         Task<Event> GetLastEvent(string aggregateName, long aggregateId);
+        Task<(Snapshot, Event[])> GetSnapshotAndEvents(string aggregateName, long aggregateId);
+        Task<long> GetSnapshotAggregateVersion(string aggregateName, long aggregateId);
         Task <long> SaveAggregate(string aggregateName, Aggregate aggregate);
         Task<long> SaveSnapshot(string aggregateName, Snapshot snapshot);
         Task<long> SaveEvent(string aggregateName, Event @event);
-        Task<AggregateEvent[]> GetAggregateEventsByAggregateTypeName(string aggregateName, string aggregateTypName, long aggregateVersion);
         Task CreateEventsAndSnapshotsTables(string aggregateName);
     }
 }
