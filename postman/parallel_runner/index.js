2515947f23752bb15a95f67432d4b80197d583aa
Roney George <georgeroney@hotmail.com>
2020-05-16 14:07:24 -0400

    added newman for parallel postman runs

---
+++ postman/parallel_runner/index.js
@@ -0,0 +1,40 @@
+const path = require("path")
+const async = require("async")
+const newman = require("newman")
+const fs = require("fs")
+
+let collectionJson = JSON.parse(fs.readFileSync("../ORLEANS_Account.postman_collection.json"));
+// when using iterationData, filter out methods that are not in csv(or JSON)
+collectionJson.item[0].item = collectionJson.item[0].item.filter(i => (i.name === "TRANSFER"));
+
+const PARALLEL_RUN_COUNT = 3
+
+const parametersForTestRun = {
+    collection: collectionJson, // collection
+    environment: path.join(__dirname, "../local.environment.json"), // env
+    globals: path.join(__dirname, "../globals.json"), //global
+    iterationData: path.join(__dirname, "../postman-runner2.csv"), //iteration
+    reporters: "cli"
+};
+
+parallelCollectionRun = function (done) {
+    newman.run(parametersForTestRun, done);
+};
+
+let commands = []
+for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
+    commands.push(parallelCollectionRun);
+}
+
+// Runs the Postman sample collections in parallel.
+async.parallel(
+    commands,
+    (err, results) => {
+        err && console.error(err);
+
+        results.forEach(function (result) {
+            var failures = result.run.failures;
+            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+                `${result.collection.name} ran successfully.`);
+        });
+    });
---
+++ postman/parallel_runner/readme.md
@@ -0,0 +1,3 @@
+# Run Postman runner files in parallel [...](https://stackoverflow.com/questions/36157105/how-to-make-multiple-requests-at-the-same-time-using-postman/41181892#41181892)
+
+```npm install && npm start```
