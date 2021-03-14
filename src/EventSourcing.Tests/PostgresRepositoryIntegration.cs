77b609c5937f515595eafda63dad98f13dca842a
Roney George <georgeroney@hotmail.com>
2021-03-13 23:46:39 -0500

    renamed test file

--- src/EventSourcing.Tests/PostgresRepositoryIntegration.cs
+++ src/EventSourcing.Tests/PostgresRepositoryIntegration.cs
@@ -5,19 +5,24 @@ using Xunit;
 
 namespace EventSourcing.Tests
 {
-    public class UnitTest1
+    public class PostgresRepositoryIntegration
     {
         [Fact]
         public async Task Test1()
         {
+            // arrange
             var postgresRepo = new PostgresRepository("host=localhost;database=EventSourcing;username=orleans;password=5544338;Enlist=false;Maximum Pool Size=90;");
             var events = new AggregateEventBase[]
             { 
                 new AggregateEventBase{ AggregateId = 3, AggregateVersion = 6744, EventVersion = 0, ParentEventId = null, RootEventId = null, Type = "TransferDebited", Data = "",  Created = DateTime.UtcNow },
                 new AggregateEventBase{ AggregateId = 3, AggregateVersion = 6745, EventVersion = 0, ParentEventId = null, RootEventId = null, Type = "TransferDebited", Data = "",  Created = DateTime.UtcNow },
             };
-            //await postgresRepo.SaveEvent("account", events[0]);
-            await postgresRepo.SaveEvents("account", events);
+
+            // act
+            var id = await postgresRepo.SaveEvents("account", events);
+
+            // assert
+            Assert.NotEqual(0, id);
         }
     }
 }
